1c9ef894306310f66f6349073dd70eed4594e3e5
Kanan Rustamli <kenanrustamli@gmail.com>
2021-08-27 21:20:10 +0400

    Version 1

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('hi');
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener('click', this.start)
+  }
+
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  }
+
+  pause = () => {
+    clearInterval(this.interval);
+  }
+
+  tick = () => {
+    const timeRemaining = parseFloat(this.durationInput.value);
+    this.durationInput.value = timeRemaining - 1;
+  }
+}
+
+const duration = document.querySelector('#duration');
+const start = document.querySelector('#start');
+const pause = document.querySelector('#pause');
+
+const timer = new Timer(duration, start, pause);
